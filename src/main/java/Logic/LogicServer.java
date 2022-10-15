483544dde397197017684e4c416fead78045afcb
Pascal Kießler <pascal.kiessler@haw-hamburg.de>
2022-10-15 11:14:59 +0200

    Methode zum suchen der spieler und findgame angefangen

--- src/main/java/Logic/LogicServer.java
+++ src/main/java/Logic/LogicServer.java
@@ -3,22 +3,26 @@ package Logic;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  *
  * @author Pascal
  */
 public class LogicServer implements TicTacToeAService {
+    private HashMap<String, String> player;
+
 
     /**
      * Constructor
      */
     public LogicServer(){
-
+        player = new HashMap<String, String>();
     }
 
     /**
-     *
+     * Server accepts client's game registration and waits for the next
+     * client to create a game between them
      * @param clientName Name of the current client
      * @return A map with three keys
      * @throws RemoteException if something went wrong
@@ -52,5 +56,27 @@ public class LogicServer implements TicTacToeAService {
     @Override
     public ArrayList<String> fullUpdate(String gameId) throws RemoteException {
         return null;
+    }
+
+    /**
+     * Decide which player starts in the game randomly.
+     * @param client_1 the first client
+     * @param client_2 the second client
+     * @return the client with the first move
+     */
+    private String startPlayer(String client_1, String client_2) {
+        Random rand = new Random();
+        int client1;
+        int client2;
+        do {
+            client1 = rand.nextInt(100);
+            client2 = rand.nextInt(100);
+        } while (client1 == client2);
+
+        if (client1 < client2){
+            return client_2;
+        } else {
+            return client_1;
+        }
     }
 }
